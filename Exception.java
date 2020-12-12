0b5fe4d35c3ebb74895d1eccf66d30464ea231b5
DENNIS MWANGI <dennistrevor06@gmail.com>
2020-12-12 19:08:41 +0000

    practice

--- Exception.java
+++ Exception.java
@@ -62,3 +62,20 @@ import java.io.FileReader;
         } 
     } 
 } 
+
+
+// Java program to demonstrate NumberFormatException 
+class  NumberFormat_Demo 
+{ 
+    public static void main(String args[]) 
+    { 
+        try { 
+            // "akki" is not a number 
+            int num = Integer.parseInt ("Emily") ; 
+  
+            System.out.println(num); 
+        } catch(NumberFormatException e) { 
+            System.out.println("Number format exception"); 
+        } 
+    } 
+} 
