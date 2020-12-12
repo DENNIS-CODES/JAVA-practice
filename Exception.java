90abece3048729c17194591d2d5d447372456762
DENNIS MWANGI <dennistrevor06@gmail.com>
2020-12-12 19:06:02 +0000

    practice

--- Exception.java
+++ Exception.java
@@ -26,4 +26,20 @@ class NullPointer_Demo
             System.out.println("NullPointerException.."); 
         } 
     } 
+}
+
+// Java program to demonstrate StringIndexOutOfBoundsException 
+class StringIndexOutOfBound_Demo 
+{ 
+    public static void main(String args[]) 
+    { 
+        try { 
+            String a = "This is like chipping "; // length is 22 
+            char c = a.charAt(24); // accessing 25th element 
+            System.out.println(c); 
+        } 
+        catch(StringIndexOutOfBoundsException e) { 
+            System.out.println("StringIndexOutOfBoundsException"); 
+        } 
+    } 
 } 
