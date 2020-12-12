c9bf9758fc4a03b7ea6def28a12fee8a1936141e
DENNIS MWANGI <dennistrevor06@gmail.com>
2020-12-12 19:09:35 +0000

    practice

--- Exception.java
+++ Exception.java
@@ -78,4 +78,20 @@ class  NumberFormat_Demo
             System.out.println("Number format exception"); 
         } 
     } 
+}
+
+// Java program to demonstrate ArrayIndexOutOfBoundException 
+class ArrayIndexOutOfBound_Demo 
+{ 
+    public static void main(String args[]) 
+    { 
+        try{ 
+            int a[] = new int[5]; 
+            a[6] = 9; // accessing 7th element in an array of 
+                      // size 5 
+        } 
+        catch(ArrayIndexOutOfBoundsException e){ 
+            System.out.println ("Array Index is Out Of Bounds"); 
+        } 
+    } 
 } 
