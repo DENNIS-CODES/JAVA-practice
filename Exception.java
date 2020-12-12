84df54001d68088d2e2b3c854044aa5d309eadeb
DENNIS MWANGI <dennistrevor06@gmail.com>
2020-12-12 19:07:02 +0000

    practice

--- Exception.java
+++ Exception.java
@@ -42,4 +42,23 @@ class StringIndexOutOfBound_Demo
             System.out.println("StringIndexOutOfBoundsException"); 
         } 
     } 
+}
+
+//Java program to demonstrate FileNotFoundException 
+import java.io.File; 
+import java.io.FileNotFoundException; 
+import java.io.FileReader; 
+ class File_notFound_Demo { 
+  
+    public static void main(String args[])  { 
+        try { 
+  
+            // Following file does not exist 
+            File file = new File("E://file.txt"); 
+  
+            FileReader fr = new FileReader(file); 
+        } catch (FileNotFoundException e) { 
+           System.out.println("File does not exist"); 
+        } 
+    } 
 } 
